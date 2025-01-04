b6474f150c55d5d1ae60a68950f4f46b94c7fc57
Lucas <lucasnrgaard@gmail.com>
2025-01-04 14:00:55 +0100

    chore: format

--- playground/tsup.config.ts
+++ playground/tsup.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["./src/index.ts"],
-  format: ["cjs", "esm"],
-  clean: true,
-  dts: true,
-  treeshake: true,
-  bundle: true,
-  outExtension(ctx) {
-    return {
-      js: ctx.format === "cjs" ? ".cjs" : ".mjs"
-    };
-  }
+	entry: ["./src/index.ts"],
+	format: ["cjs", "esm"],
+	clean: true,
+	dts: true,
+	treeshake: true,
+	bundle: true,
+	outExtension(ctx) {
+		return {
+			js: ctx.format === "cjs" ? ".cjs" : ".mjs",
+		};
+	},
 });
